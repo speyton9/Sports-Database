95173284b2aad935a61fa5f04c70c4253a2f067f
Steven Peyton <60023515+speyton9@users.noreply.github.com>
2022-11-11 13:52:47 -0500

    Update default_values.py

--- default_values.py
+++ default_values.py
@@ -1,8 +1,8 @@
 # Default path to export NBA summary
-nba_export = f'C:\\Users\\Steven\\Desktop\\NBAstats.csv'
+nba_export = f'C:\\YOUR\\DEFAULT\\PATH\\HERE.csv'
 
 # Default path to export NHL summary
-nhl_export = f'C:\\Users\\Steven\\Desktop\\NHLstats.csv'
+nhl_export = f'C:\\YOUR\\DEFAULT\\PATH\\HERE.csv'
 
 # Default NBA DB connection
 nba_db_connection = 'nba-stats.db'
